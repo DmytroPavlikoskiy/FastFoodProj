1ec6771cd21a4babd80de5586e92844f9fb2d040
G1zmo <evdokjeka1512@gmail.com>
2026-01-04 10:53:34 -0500

    Add Comments model to payment/models.py

--- payment/models.py
+++ payment/models.py
@@ -10,3 +10,10 @@ class Payment(db.Model):
     status = db.Column(db.String(20))
     transcation_id = db.Column(db.String(100))
     
+class Comments(db.Model):
+    id = db.column(db.Integer, primary_key=True)
+    User_id = db.Column(db.Integer, nullable=False)
+    product_id = db.Column(db.Integer, nullable=False)
+    text = db.Column(db.text, nullable=False)
+    rating = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
