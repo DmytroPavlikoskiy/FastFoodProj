c57b3d7929be756a5fdd56ad622e759dd57b3f21
maksimkarbovnik <getuch375@gmail.com>
2026-01-07 19:25:39 +0200

    add payment views and payment models

---
+++ payment/models.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+from db_config.db_manager import db
+
+
+class Payment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    order_id = db.Column(db.Integer, nullable=False)
+    status = db.Column(db.String(20))
+    transcation_id = db.Column(db.String(100))
+    
+class Comments(db.Model):
+    id = db.column(db.Integer, primary_key=True)
+    User_id = db.Column(db.Integer, nullable=False)
+    product_id = db.Column(db.Integer, nullable=False)
+    text = db.Column(db.text, nullable=False)
+    rating = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
--- payment/views.py
+++ payment/views.py
@@ -1,7 +1,63 @@
-from flask import Blueprint
+from flask import Blueprint, redirect, url_for, request, abort
+from models import db, Payment, Comments
+import uuid
+
 
 payment_bp = Blueprint('payment', __name__)
 
 @payment_bp.route('/example_url')
 def example():
     pass
+
+
+def process_payment(order_id):
+    transaction_id = str(uuid.uuid4())
+
+    payment = Payment(
+        order_id=order_id,
+        status = "success",
+        transaction_id=transaction_id
+    )
+
+
+    db.session.add(payment)
+    db.session.commit()
+
+    return redirect(url_for('success_page'))
+
+
+@payment_bp.route('/comment/<int:product_id>/<int:user_id>', methods=['POST'])
+def add_comment(product_id, user_id):
+    payment = Payment.query.filter_by(status="success").first()
+    if not payment:
+        abort(403, description="Коментар можна залишити тільки після покупки!")
+
+
+    text = request.form.get("text")
+    rating = request.form.get("rating")
+
+
+
+    if not text or not rating:
+        abort(400, description="Заповніть всі поля коментаря!")
+
+    try:
+        rating = int(rating)
+        if rating < 1 or rating > 5:
+            abort(400, description="Рейтинг має бути від 1 до 5")
+
+    except ValueError:
+         abort(400, description="Рейтинг має бути числом")
+
+    comment = Comments(
+        user_id=user_id,
+        product_id=product_id,
+        text=text,
+        rating=rating
+    )
+
+    db.session.add(comment)
+    db.session.commit()
+
+
+    return "Коментар додано успішно"
